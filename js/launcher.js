be65c46907af41b3bf4768a6722b560d747fa59c
Kevin22457 <76512912+Kevin22457@users.noreply.github.com>
2021-05-10 03:40:22 -0700

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,25 @@
+class Launcher {
+    constructor(body,point) {
+        var options = {
+            bodyA:body,
+            pointB:point,
+            stiffness:0.04,
+            length:30
+        }
+        this.chain = Constraint.create(options)
+        World.add(world,this.chain);
+    }
+    display() {
+        if (this.chain.bodyA) {
+            var pointA = this.chain.bodyA.position
+            var pointB = this.chain.pointB
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    fly(){
+      this.chain.bodyA = null
+    }
+    attach(body){
+      this.chain.bodyA = body
+    }
+}
